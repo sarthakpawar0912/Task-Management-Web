251e6e92b77da09d4aca797ab2dc27dd60c64bb4
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-15 22:55:57 +0530

    Fixed bugs and updated code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './auth/components/login/login.component';
 import { SignupComponent } from './auth/components/signup/signup.component';
 
   const routes: Routes = [
+   
     { path: "", redirectTo: "login", pathMatch: "full" }, // ✅ Keep only ONE default route
     { path: "login", component: LoginComponent },
     { path: "signup", component: SignupComponent },
     { path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(e => e.AdminModule) },
     { path: "employee", loadChildren: () => import("./modules/employee/employee.module").then(e => e.EmployeeModule) },
+  
   ];
   
 @NgModule({
