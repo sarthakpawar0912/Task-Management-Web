7645b9f2635518aa907427489df29aa59db0552d
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-15 23:22:35 +0530

    Fixed bugs and updated code

--- src/app/modules/employee/employee-routing.module.ts
+++ src/app/modules/employee/employee-routing.module.ts
@@ -4,9 +4,12 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { ViewTaskDetailsComponent } from './component/view-task-details/view-task-details.component';
 
 const routes: Routes = [
+
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'view-details/:id', component: ViewTaskDetailsComponent },];
-@NgModule({
+  { path: 'view-details/:id', component: ViewTaskDetailsComponent },
+];
+
+  @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
